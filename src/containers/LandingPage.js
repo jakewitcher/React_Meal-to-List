26632ce3a7faefc31aff518fb1c580d91d186658
Jake Witcher <jjwitcher09@gmail.com>
2018-12-23 20:24:10 -0500

    new color palette, styling for landing page desktop

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
     return (
-        <div className="landingpage">
-            <h1 className="landingpage__title">Meal to List</h1>
-            <h3 className="landingpage__subtitle">Meal Planning Made Easy</h3>
-            <Link to="meals/create" className="landingpage__button--div">
-                <button className="landingpage__button">Get started!</button>
-            </Link>
+        <div class="container container__landingpage">
+            <div className="landingpage">
+                <h1 className="landingpage__title">Meal to List</h1>
+                <h3 className="landingpage__subtitle">Meal Planning Made Easy</h3>
+                <Link to="meals/create" className="landingpage__button--div">
+                    <button className="landingpage__button">Get started!</button>
+                </Link>
+            </div>
         </div>
     );
 }
